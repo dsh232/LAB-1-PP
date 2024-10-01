885b6504f5c77453b3f07a5ba35df7e96bfc6a2c
Lenovo <Lenovo@DESKTOP-D91BPDT>
2024-10-01 21:45:32 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    int length;
+    std::cout << "what length of your array? " << std::endl;
+    std::cin >> length;
+    std::cout << "what numbers in your array? ";
+
+    int num0 = 0;
+
+    int *masiv = new int(length); //Выделение памяти на динамический массив. (пример с мусором в комнате и тазиком)
+    for (int i = 0; i < length; i++) {
+        std::cin >> masiv[i];
+        if (masiv[i] == 0)  num0++;
+    }
+
+
+    for (int j = 0; j < num0; j++) {
+        for (int i = 1; i < length; i++) {
+            if (masiv[i - 1] == 0) {
+
+                masiv[i - 1] = masiv[i];
+                masiv[i] = 0;
+            }
+        }
+    }
+
+    std::cout << std::endl;
+    for (int i = 0; i < length; i++) {
+        std::cout << masiv[i] << " ";
+    }    
+}   
