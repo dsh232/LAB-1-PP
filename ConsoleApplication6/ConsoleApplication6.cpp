d938fe13fea6ed37ba25c2f8495aee6ee2932ab9
dsh232 <nifitca.lex@gmail.com>
2024-10-01 23:03:28 +0400

    yo bozos

--- ConsoleApplication6/ConsoleApplication6.cpp
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -1,21 +1,35 @@
 ﻿
 #include <iostream>
 
+int input_int(bool is_neg) { // создание функции на проверку int (если буквы, запятые и тд) // is_neg - отриц. ли число true/false
+    int inputInt;
+    while (!(std::cin >> inputInt)) { // не инт проверка
+        std::cout << "Can you please put CORRECT value? Try again. " << std::endl;
+        std::cin.clear(); // чистим поток. пример с гречкой и зубом и стоматологом (clear)
+    }
+    if (!is_neg && inputInt < 0)  // для проверки при чтении размера массива. (отриц. числа мимо)
+    {
+        std::cout << "Try numbers that > or = 0. " << std::endl;
+        return input_int(false);
+    } 
+
+    return inputInt;
+}
+
 int main()
 {
     int length;
-    std::cout << "what length of your array? " << std::endl;
-    std::cin >> length;
-    std::cout << "what numbers in your array? ";
+    std::cout << "what length of your array? " << std::endl; //cin,cout потоки выводп/ввода. std группа пространств имен
+    length = input_int(false);
+    std::cout << "what numbers in your array? " << std::endl;
 
     int num0 = 0;
 
     int *masiv = new int(length); //Выделение памяти на динамический массив. (пример с мусором в комнате и тазиком)
     for (int i = 0; i < length; i++) {
-        std::cin >> masiv[i];
+        masiv[i] = input_int(true);
         if (masiv[i] == 0)  num0++;
     }
-
 
     for (int j = 0; j < num0; j++) {
         for (int i = 1; i < length; i++) {
@@ -27,7 +41,8 @@ int main()
         }
     }
 
-    std::cout << std::endl;
+    std::cout << "Your array with the zero elements moved to the end of the array: ";
+
     for (int i = 0; i < length; i++) {
         std::cout << masiv[i] << " ";
     }    
